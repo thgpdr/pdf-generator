435845a6f85441422dd982f1f32799c42bc7be19
pho <pho@vortx.com.br>
2020-02-03 16:51:02 -0300

    upload

---
+++ pdfTestFront/teste/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3005',
+});
+
+async function getPDF() {
+  return api.get('/pdf',
+    {
+      responseType: 'arraybuffer',
+      headers: {
+        'Accept': 'application/pdf',
+      }
+    });
+}
+
+function savePDF() {
+  return getPDF().then(response => {
+    console.log(response)
+    const blob = new Blob([response.data],
+      {
+        type: 'application/pdf'
+      });
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = 'your-file.pdf';
+    link.click()
+  })
+}
+
+function App() {
+  // savePDF().then(response => {
+  //   console.log(response);
+  // })
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={savePDF}> PDF CLIQUE ME</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
