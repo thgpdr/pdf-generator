038cfae80150b99d4874c696a86699e93fbefb34
pho <pho@vortx.com.br>
2020-02-05 11:01:35 -0300

    checkpoint semana facil para os inimigos

--- pdfBack/getPdf.js
+++ pdfBack/getPdf.js
@@ -1,92 +1,79 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 async function printPDF(tipoFundo, cnpjFundo) {
-  tipoFundo = 'FICFMMerc';
-  cnpjFundo = '29625962000118';
-  const browser = await puppeteer.launch({ headless: false, defaultViewport: null, args: ['--window-size=1920, 1080'] });
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 0.2 })
-  await page.goto('http://localhost:3000/', {
-    waitUntil: 'networkidle0',
+  const pdfs = [];
+  // parametros que o front deve enviar -valores mocados para teste
+  tipoFundo = "FICFMMerc";
+  cnpjFundo = "29625962000118";
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+    args: ["--window-size=1920, 1080"]
   });
-
-  // const session = await page.target().createCDPSession();
-  // await session.send('Emulation.setPageScaleFactor', {
-  //   pageScaleFactor: 0.5, // 400%
-  // });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 0.2 });
+  // url do portal
+  await page.goto("http://localhost:81/", {
+    waitUntil: "networkidle0"
+  });
 
   let HTML = "<style>";
   await page.click(`#${tipoFundo} > div`).then(async () => {
     await page.click(`#${cnpjFundo}`).then(async () => {
-      let classes = await page.evaluate(async () => document.querySelector(`#classes`).innerText)
-      classes = classes.split('\n')
-      console.log(classes)
+      let classes = await page.evaluate(
+        async () => document.querySelector(`#classes`).innerText
+      );
+      classes = classes.split("\n");
+      // para cada classe no fundo
       classes.forEach(async classe => {
-        await page.select('#classes', classe ).then(async () => {
+        await page.select("#classes", classe).then(async () => {
           await page.click(`#ativos`);
+          // pego todos os estilos aplicados na página dinamicamente
           HTML += await page.evaluate(async () => {
             let style;
-            let styles = document.getElementsByTagName('style');
-            for(var i=0; i< styles.length; i++) {
+            let styles = document.getElementsByTagName("style");
+            for (var i = 0; i < styles.length; i++) {
               style += styles[i].innerHTML;
             }
-            
-            style += "</style>"
+
+            style += "</style>";
             return style;
-          })
-    
-          HTML += await page.evaluate(async () => document.querySelector('.tabelas-fundo').outerHTML)
-        })
-      })
-    })
-  })
-  
+          });
 
+          // pego as tabelas de ativos/passivos
+          HTML += await page.evaluate(
+            async () => document.querySelector(".tabelas-fundo").outerHTML
+          );
 
-  //   await page.click(`#ativos`)
-    
+          // renderizo as tabelas junto ao css
+          await page.setContent(HTML);
 
+          // escondo alguns elementos que não devem aparecem no pdf
+          await page.addStyleTag({
+            content: "#carteira {display: none;}"
+          });
+          await page.addStyleTag({
+            content: ".MTableToolbar-searchField-38 {display: none;}"
+          });
 
-  //   // let html = await page.evaluate(async () => document.querySelector('.tabelas-fundo').outerHTML)
-    
-  //   //   const tabelas = document.querySelector('.tabelas-fundo');
-  //   //   return JSON.parse(JSON.stringify(getComputedStyle(tabelas)));
-  //   // })
-  //   // classes.forEach(async classe => {
-  //   // await page.select('#classes', classe );
-  //   // // await page.click(`#ativos`).then(async () => {
-
-  //   // // })
-  //   // })
+          // adiciono cada pdf de classe em um array
+          pdfs.push(
+            await page.pdf({
+              format: "A4",
+              printBackground: true,
+              landscape: true
+            })
+          );
+        });
+      });
+    });
+  });
   //   // await page.addStyleTag({
   //   //   content: "@page:first {margin-top: 0;} body {margin-top: 1cm;}"
   //   // });
 
-    
-
-
-  //   // console.log(css);
-  //   await page.setContent(HTML)
-  //   await page.addStyleTag({
-  //     content: "#carteira {display: none;}"
-  //   })
-  //   await page.addStyleTag({
-  //     content: ".MTableToolbar-searchField-38 {display: none;}"
-  //   })
-  // });
-  
-
-  const pdfAtivos = await page.pdf({ format: 'A4', printBackground: true, landscape: true });
-
   await browser.close();
-  return pdfAtivos;
+  return pdfs;
 }
 
 module.exports = printPDF;
-
-  // await page.click(`[id='${cnpjFundo}']`)
-      // let classes = await page.evaluate(async () => document.querySelector(`#classes`))
-      // classes.forEach(async classe => {
-      //   await page.click(`#${classe}`);
-      //   await page.click('#ativos');
-      // })
--- pdfTestFront/teste/src/App.js
+++ pdfTestFront/teste/src/App.js
@@ -1,33 +1,31 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3005',
+  baseURL: "http://localhost:3005"
 });
 
 async function getPDF() {
-  return api.get('/pdf',
-    {
-      responseType: 'arraybuffer',
-      headers: {
-        'Accept': 'application/pdf',
-      }
-    });
+  return api.get("/pdf", {
+    responseType: "arraybuffer",
+    headers: {
+      Accept: "application/pdf"
+    }
+  });
 }
 
 function savePDF() {
   return getPDF().then(response => {
-    console.log(response)
-    const blob = new Blob([response.data],
-      {
-        type: 'application/pdf'
-      });
-    const link = document.createElement('a');
+    console.log(response);
+    const blob = new Blob([response.data], {
+      type: "application/pdf"
+    });
+    const link = document.createElement("a");
     link.href = window.URL.createObjectURL(blob);
-    link.download = 'your-file.pdf';
-    link.click()
-  })
+    link.download = "your-file.pdf";
+    link.click();
+  });
 }
 
 function App() {
